6a197c51fff6a8ce24e81e0a5c487abeae887148
ok2qw66 <ok2qw66@naver.com>
2020-08-26 14:30:37 +0900

    test song_insert

--- plist/song_insert.py
+++ plist/song_insert.py
@@ -2,7 +2,7 @@ import pymysql, os, json
 
 # read JSON file which is in the next parent folder
 file = os.path.abspath('../music.json')
-json_data=open(file).read()
+json_data = open(file).read()
 json_obj = json.loads(json_data)
 
 
@@ -18,7 +18,7 @@ def validate_string(val):
 
 
 # connect to MySQL
-con = pymysql.connect(host = '192.168.0.12',user = 'project',passwd = 'project',db = 'project_db')
+con = pymysql.connect(host='http://172.30.1.31',user = 'project',passwd = 'project',db = 'project_db')
 cursor = con.cursor()
 print(con,cursor)
 
@@ -29,12 +29,14 @@ for i, item in enumerate(json_obj):
     song_url = validate_string(item.get("song_url", None))
     song_genre = str(item.get("song_genre", None))
     song_tag = str(item.get("song_tag", None))
+    song_start = str(item.get("song_start", None))
+    song_end = str(item.get("song_end", None))
     song_detail = validate_string(item.get("song_detail", None))
 
     # print(song_title, type(song_title))
     # print(song_tag, type(song_tag))
 
-    cursor.execute("INSERT INTO plist_song (song_title,song_artist,song_url,song_genre,song_tag,song_detail) VALUES (%s,%s,%s,%s,%s,%s)", (song_title,song_artist,song_url,song_genre,song_tag,song_detail))
+    cursor.execute("INSERT INTO plist_song (song_title,song_artist,song_url,song_genre,song_tag,song_start,song_end,song_detail) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (song_title,song_artist,song_url,song_genre,song_tag,song_start,song_end,song_detail))
 
 con.commit()
 con.close()
