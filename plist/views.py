7fa017b9cb05c4122325a6fb403244349af4ddb7
ok2qw66 <ok2qw66@naver.com>
2020-09-01 00:39:39 +0900

    from other playlist to my playlist

--- plist/views.py
+++ plist/views.py
@@ -61,6 +61,20 @@ def list_new(request):
         return render(request, 'plist/list_new.html', {'form':form, 'song_list':song_list})
 
 
+# 다른 사람 플레이 리스트 -> 내 플레이리스트에 추가
+def list_copy(request,pk):
+    # 다른 사람 플레이리스트 가져오기
+    playlist = get_object_or_404(Playlist,pk=pk)
+    # 로그인 사용자 계정으로 플레이리스트 db에 추가
+    playlist = Playlist.objects.create(play_title=playlist.play_title,
+                            author=request.user,
+                            play_list=playlist.play_list,
+                            play_detail=str(playlist.author)+'의 플레이리스트 copy',
+                            )
+
+    return redirect('playlist')
+
+
 # 로그인 페이지
 def login(request):
     if request.method == "POST":
@@ -98,20 +112,17 @@ def song_new(request):
         form = SongForm(request.POST)
         if form.is_valid():
             # 파일뽑아내는 작업
-            # download_video_and_subtitle(form.cleaned_data['song_url'], form.cleaned_data['song_title'])
+            download_video_and_subtitle(form.cleaned_data['song_url'], form.cleaned_data['song_title'])
 
             # 파일 자르는 작업
-            # song_slice(form.cleaned_data['song_title'],form.cleaned_data['song_start'],form.cleaned_data['song_end'])
+            song_slice(form.cleaned_data['song_title'],form.cleaned_data['song_start'],form.cleaned_data['song_end'])
 
             # 썸네일 만드는 작업
-            # verify = get_thumbnail(form.cleaned_data['song_artist'])
-            # if verify:
-            #     thumbnail = form.cleaned_data['song_artist']
-            # else:
-            #     thumbnail = 'default'
-
-            # 테스트용
-            thumbnail = form.cleaned_data['song_artist']
+            verify = get_thumbnail(form.cleaned_data['song_artist'])
+            if verify:
+                thumbnail = form.cleaned_data['song_artist']
+            else:
+                thumbnail = 'default'
 
             # db에 넣기
             song = Song.objects.create(song_title=form.cleaned_data['song_title'],\
@@ -162,10 +173,6 @@ def event(request):
 
 def blog(request):
     return render(request, 'plist/blog.html')
-
-
-def contact(request):
-    return render(request, 'plist/contact.html')
 
 
 def element(request):
